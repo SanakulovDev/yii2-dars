7befa476b21c7c33eb89a37c110e5688abf99e3e
= <=>
2021-11-12 10:43:33 +0500

    40

--- frontend/models/Company.php
+++ frontend/models/Company.php
@@ -87,7 +87,7 @@ class Company extends \yii\db\ActiveRecord
     public function upload($image)     
     {
         if ($image !== null) {
-            $dir = Yii::getAlias('@web')."/uploads/company/";
+            $dir = Yii::getAlias('@frontend')."/web/uploads/company/";
             $image_name =time();
             $image_name .= '.'.$image->extension;
             if ($image->saveAs($dir.$image_name)) {
