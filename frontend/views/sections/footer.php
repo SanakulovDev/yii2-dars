76a2f36579200de0aa22108d02eac51618eaf409
= <=>
2021-11-12 20:48:38 +0500

    72

--- frontend/views/sections/footer.php
+++ frontend/views/sections/footer.php
@@ -1,58 +1,100 @@
 <footer class="site-footer">
 
-      <a href="#top" class="smoothscroll scroll-top">
+    <a href="#top" class="smoothscroll scroll-top">
         <span class="icon-keyboard_arrow_up"></span>
-      </a>
+    </a>
 
-      <div class="container">
+    <div class="container">
         <div class="row mb-5">
-          <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3><?=Yii::t('app','Search Trending')?></h3>
-            <ul class="list-unstyled">
-              <li><a href="#"><?=Yii::t('app','Web Design')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Graphic Design')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Web Developers')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Python')?></a></li>
-              <li><a href="#"><?=Yii::t('app','HTML5')?></a></li>
-              <li><a href="#"><?=Yii::t('app','CSS3')?></a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3><?=Yii::t('app','Company name')?></h3>
-            <ul class="list-unstyled">
-              <li><a href="#"><?=Yii::t('app','About Us')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Career')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Blog')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Resources')?></a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3><?=Yii::t('app','Support')?></h3>
-            <ul class="list-unstyled">
-              <li><a href="#"><?=Yii::t('app','Support')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Privacy')?></a></li>
-              <li><a href="#"><?=Yii::t('app','Terms of Service')?></a></li>
-            </ul>
-          </div>
-          <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3><?=Yii::t('app','Contact')?></h3>
-            <div class="footer-social">
-              <a href="#"><span class="icon-facebook"></span></a>
-              <a href="#"><span class="icon-twitter"></span></a>
-              <a href="#"><span class="icon-instagram"></span></a>
-              <a href="#"><span class="icon-linkedin"></span></a>
+            <div class="col-6 col-md-3 mb-4 mb-md-0">
+                <h3><?= Yii::t('app', 'Search Trending') ?></h3>
+                <ul class="list-unstyled">
+                    <li><a href="#"><?= Yii::t('app', 'Web Design') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Graphic Design') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Web Developers') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Python') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'HTML5') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'CSS3') ?></a></li>
+                </ul>
             </div>
-          </div>
+            <div class="col-6 col-md-3 mb-4 mb-md-0">
+                <h3><?= Yii::t('app', 'Company name') ?></h3>
+                <ul class="list-unstyled">
+                    <li><a href="#"><?= Yii::t('app', 'About Us') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Career') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Blog') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Resources') ?></a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-3 mb-4 mb-md-0">
+                <h3><?= Yii::t('app', 'Support') ?></h3>
+                <ul class="list-unstyled">
+                    <li><a href="#"><?= Yii::t('app', 'Support') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Privacy') ?></a></li>
+                    <li><a href="#"><?= Yii::t('app', 'Terms of Service') ?></a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-3 mb-4 mb-md-0">
+                <h3><?= Yii::t('app', 'Contact') ?></h3>
+                <div class="footer-social">
+                    <a href="#"><span class="icon-facebook"></span></a>
+                    <a href="#"><span class="icon-twitter"></span></a>
+                    <a href="#"><span class="icon-instagram"></span></a>
+                    <a href="#"><span class="icon-linkedin"></span></a>
+                </div>
+            </div>
         </div>
 
         <div class="row text-center">
-          <div class="col-12">
-            <p class="copyright"><small>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></small></p>
-          </div>
+            <div class="col-12">
+                <p class="copyright"><small>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+                        All rights reserved | This template is made with <i class="icon-heart text-danger"
+                                                                            aria-hidden="true"></i> by <a
+                                href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></small>
+                </p>
+            </div>
         </div>
-      </div>
-    </footer>
+    </div>
+    <!-- START WWW.UZ TOP-RATING -->
+    <SCRIPT language="javascript" type="text/javascript">
+        <!--
+        top_js = "1.0";
+        top_r = "id=46019&r=" + escape(document.referrer) + "&pg=" + escape(window.location.href);
+        document.cookie = "smart_top=1; path=/";
+        top_r += "&c=" + (document.cookie ? "Y" : "N")
+        //-->
+    </SCRIPT>
+    <SCRIPT language="javascript1.1" type="text/javascript">
+        <!--
+        top_js = "1.1";
+        top_r += "&j=" + (navigator.javaEnabled() ? "Y" : "N")
+        //-->
+    </SCRIPT>
+    <SCRIPT language="javascript1.2" type="text/javascript">
+        <!--
+        top_js = "1.2";
+        top_r += "&wh=" + screen.width + 'x' + screen.height + "&px=" +
+            (((navigator.appName.substring(0, 3) == "Mic")) ? screen.colorDepth : screen.pixelDepth)
+        //-->
+    </SCRIPT>
+    <SCRIPT language="javascript1.3" type="text/javascript">
+        <!--
+        top_js = "1.3";
+        //-->
+    </SCRIPT>
+    <SCRIPT language="JavaScript" type="text/javascript">
+        <!--
+        top_rat = "&col=0063AF&t=ffffff&p=E6850F";
+        top_r += "&js=" + top_js + "";
+        document.write('<a href="http://www.uz/ru/res/visitor/index?id=46019" target=_top><img src="http://cnt0.www.uz/counter/collect?' + top_r + top_rat + '" width=88 height=31 border=0 alt="Топ рейтинг www.uz"></a>')//-->
+    </SCRIPT>
+    <NOSCRIPT><A href="http://www.uz/ru/res/visitor/index?id=46019" target=_top><IMG height=31
+                                                                                     src="http://cnt0.www.uz/counter/collect?id=46019&pg=http%3A//uzinfocom.uz&&col=0063AF&amp;t=ffffff&amp;p=E6850F"
+                                                                                     width=88 border=0
+                                                                                     alt="Топ рейтинг www.uz"></A>
+    </NOSCRIPT><!-- FINISH WWW.UZ TOP-RATING -->
+</footer>
   
