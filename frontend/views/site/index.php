818fc45fec5f3047f8faf499bdf3d9acc9103150
= <=>
2021-11-17 14:09:55 +0500

    174

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -229,7 +229,7 @@ $this->title = 'My Yii Application';
 
         series: [{
             data: data,
-            name: <?=$vacancy->company->name?>,
+            name: <?php echo $vacancy?>,
             states: {
                 hover: {
                     color: '#BADA55'
