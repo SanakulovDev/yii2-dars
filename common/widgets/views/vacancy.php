0d8904e6d25fc207e53e724a0a0b039d25917a72
= <=>
2021-11-29 10:38:03 +0500

    286

--- common/widgets/views/vacancy.php
+++ common/widgets/views/vacancy.php
@@ -9,9 +9,9 @@ $description = 'description_' . Yii::$app->language;
 
     <ul class="job-listings mb-5 mr-auto row " style="flex-direction: column; align-content: flex-end; ">
         <?php foreach ($vacancy as $item): ?>
-            <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+            <li class="job-listing d-block d-sm-flex pb-0 pb-sm-0 align-items-center">
                 <a href="/site/vacancy-views?id=<?= $item->id ?>&get=false"></a>
-                <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between p-2">
+                <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between">
                     <div class="job-listing-position custom-width  mb-3 mb-sm-0">
                         <h6><?= $item->profession? $item->profession->$lang:''?></h6>
                         <?php if ($item->$description): ?>
