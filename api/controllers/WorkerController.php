23cb048536031845a51e9997e90ef0580ebee7b9
= <=>
2021-12-03 17:51:46 +0500

    325

--- api/controllers/WorkerController.php
+++ api/controllers/WorkerController.php
@@ -2,23 +2,46 @@
 
 namespace api\controllers;
 
+use api\models\Worker;
+use api\models\User;
+use yii\data\ActiveDataProvider;
+use yii\web\Response;
 
 class WorkerController extends \yii\rest\ActiveController
 {
     public $modelClass = "api\models\Worker";
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+                'Origin' => ['*'],
+            ],
 
+        ];
+        return $behaviors;
+    }
     public function actions()
     {
         $actions = parent::actions();
+        unset($actions['index']);
+//        unset($actions['create']);
         return $actions;
     }
-    public function behaviors()
-    {
-        return [
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::class,
-            ],
-        ];
-    }
 
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Worker::find(),
+//            'pagination' => [
+//                'pageSize' => 2
+//            ]
+        ]);
+        return $dataProvider;
+    }
 }
--- api/models/Worker.php
+++ api/models/Worker.php
@@ -2,6 +2,9 @@
 
 namespace api\models;
 
+use yii\helpers\Url;
+use yii\web\Link;
+
 class Worker extends \frontend\models\Worker
 {
     public function fields()
@@ -9,6 +12,23 @@ class Worker extends \frontend\models\Worker
         return [
             'firstname',
             'lastname'
+        ];
+    }
+
+    public function extraFields()
+    {
+        return [
+            'userId',
+        ];
+    }
+
+    public function getLinks()
+    {
+        return [
+            Link::REL_SELF => Url::to(['region/view', 'id' => $this->id], true),
+            'edit' => Url::to(['region/view', 'id' => $this->id], true),
+            'profile' => Url::to(['region/profile/view', 'id' => $this->id], true),
+            'index' => Url::to(['regions'], true),
         ];
     }
 }
